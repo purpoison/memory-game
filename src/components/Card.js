2b537968ebd4dd2cb5aa2817fbcf12fc12ebffbb
Tetiana <shahdi007@gmail.com>
2023-02-06 12:35:07 -0500

    finish

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,8 +1,10 @@
 import React from 'react'
 
 export default function Card({item, id, handle}) {
+  const itemClass = item.stat ? " active " + item.stat : '';
+
   return (
-    <div className='card' onClick={() => handle(id)}>
+    <div className={'card' + itemClass} onClick={() => handle(id)}>
         <img src={item.img} alt=''/>
     </div>
   )
--- src/components/Cards.js
+++ src/components/Cards.js
@@ -21,12 +21,45 @@ export default function Cards() {
         { id: 8, img: '/img/8.png', stat: ''},
         { id: 8, img: '/img/8.png', stat: ''},
     ].sort(() => Math.random() - 0.5))
+      
+    const [prev, setPrev] = useState(-1);
+    
+  function check(current){
+    if(items[current].id == items[prev].id){
+      items[current].stat = 'correct';
+      items[prev].stat = 'correct';
+      setItems([...items]);
+      setPrev(-1);
+    }else{
+      items[current].stat = 'wrong';
+      items[prev].stat = 'wrong';
+      setItems([...items]);
+      setTimeout(() => {
+        items[current].stat = '';
+        items[prev].stat = '';
+        setItems([...items]);
+        setPrev(-1);
+      }, 1000)
+    }
+  }  
+  function ifEnd(){
+    let ifEnd = 0;
+    items.forEach(item => {
+      if(item.stat != ''){
+        return ifEnd++;
+      }
+      console.log(ifEnd)
+    })
+  }
 
   function handle(id){
-    alert(id)
-    // const newItem = items.map(item => {
-
-    // })
+    if(prev === -1){
+      items[id].stat = 'active';
+      setItems([...items]);
+      setPrev(id);
+    }else{
+      check(id)
+    }
   }
     
   return (
